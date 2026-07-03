81fccc2cf68e86f3f42de5386d96aac8f710750e
Jeevika <151841112+jeevharsh@users.noreply.github.com>
2026-07-03 09:54:53 +0530

    Delete SignalProcessing.h

--- SignalProcessing.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIGNALPROCESSING_H
-#define SIGNALPROCESSING_H
-
-void initializeSensor(int pin);
-float readSensorVoltage();
-float calculateBilirubin(float voltage);
-float calculateGlucose(float voltage);
-
-#endif
